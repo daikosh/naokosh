2afbcfaae0053b235ea5e1d966b2e7a1df9f8dcf
daikoshg@gmail.com <daikoshg@gmail.com>
2022-02-20 22:16:38 +0900

    aws

--- naokosh/settings.py
+++ naokosh/settings.py
@@ -120,7 +120,7 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
-    STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME + 'naokosh/'
     STATIC_FILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
